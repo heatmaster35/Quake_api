c3fda4e86724af31f416c2b90cfc177f567726d2
heatmaster35 <leogomez176@gmail.com>
2016-03-05 14:21:01 -0800

    Create Example.java
    Json with getters and setters

---
+++ Example.java
@@ -0,0 +1,329 @@
+package com.example.shobhit.findrestaurant.response;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by leogo_000 on 3/2/2016.
+ */
+//-----------------------------------com.example.Example.java-----------------------------------
+
+//@Generated("org.jsonschema2pojo")
+public class Example {
+
+    @SerializedName("response")
+    @Expose
+    private Integer response;
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("count")
+    @Expose
+    private Integer count;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("link")
+    @Expose
+    private String link;
+    @SerializedName("source")
+    @Expose
+    private String source;
+    @SerializedName("north")
+    @Expose
+    private Double north;
+    @SerializedName("west")
+    @Expose
+    private Double west;
+    @SerializedName("lat")
+    @Expose
+    private Double lat;
+    @SerializedName("lng")
+    @Expose
+    private Double lng;
+    @SerializedName("depth")
+    @Expose
+    private Integer depth;
+    @SerializedName("mag")
+    @Expose
+    private Double mag;
+    @SerializedName("time")
+    @Expose
+    private String time;
+    @SerializedName("timestamp")
+    @Expose
+    private Integer timestamp;
+
+    /**
+     *
+     * @return
+     * The response
+     */
+    public Integer getResponse() {
+        return response;
+    }
+
+    /**
+     *
+     * @param response
+     * The response
+     */
+    public void setResponse(Integer response) {
+        this.response = response;
+    }
+
+    /**
+     *
+     * @return
+     * The message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     *
+     * @param message
+     * The message
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     *
+     * @return
+     * The count
+     */
+    public Integer getCount() {
+        return count;
+    }
+
+    /**
+     *
+     * @param count
+     * The count
+     */
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    /**
+     *
+     * @return
+     * The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     * The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     * The title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     *
+     * @param title
+     * The title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     *
+     * @return
+     * The link
+     */
+    public String getLink() {
+        return link;
+    }
+
+    /**
+     *
+     * @param link
+     * The link
+     */
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    /**
+     *
+     * @return
+     * The source
+     */
+    public String getSource() {
+        return source;
+    }
+
+    /**
+     *
+     * @param source
+     * The source
+     */
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    /**
+     *
+     * @return
+     * The north
+     */
+    public Double getNorth() {
+        return north;
+    }
+
+    /**
+     *
+     * @param north
+     * The north
+     */
+    public void setNorth(Double north) {
+        this.north = north;
+    }
+
+    /**
+     *
+     * @return
+     * The west
+     */
+    public Double getWest() {
+        return west;
+    }
+
+    /**
+     *
+     * @param west
+     * The west
+     */
+    public void setWest(Double west) {
+        this.west = west;
+    }
+
+    /**
+     *
+     * @return
+     * The lat
+     */
+    public Double getLat() {
+        return lat;
+    }
+
+    /**
+     *
+     * @param lat
+     * The lat
+     */
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    /**
+     *
+     * @return
+     * The lng
+     */
+    public Double getLng() {
+        return lng;
+    }
+
+    /**
+     *
+     * @param lng
+     * The lng
+     */
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    /**
+     *
+     * @return
+     * The depth
+     */
+    public Integer getDepth() {
+        return depth;
+    }
+
+    /**
+     *
+     * @param depth
+     * The depth
+     */
+    public void setDepth(Integer depth) {
+        this.depth = depth;
+    }
+
+    /**
+     *
+     * @return
+     * The mag
+     */
+    public Double getMag() {
+        return mag;
+    }
+
+    /**
+     *
+     * @param mag
+     * The mag
+     */
+    public void setMag(Double mag) {
+        this.mag = mag;
+    }
+
+    /**
+     *
+     * @return
+     * The time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     *
+     * @param time
+     * The time
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     *
+     * @return
+     * The timestamp
+     */
+    public Integer getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     *
+     * @param timestamp
+     * The timestamp
+     */
+    public void setTimestamp(Integer timestamp) {
+        this.timestamp = timestamp;
+    }
+
+}
